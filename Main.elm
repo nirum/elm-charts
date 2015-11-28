a543a5bc1a7a1cd31fa55cbac3d4c993d0b299df
Niru Maheswaranathan <nirum@stanford.edu>
2015-11-28 01:46:28 -0500

    simple sine wave up and running

--- Main.elm
+++ Main.elm
@@ -3,6 +3,9 @@ import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 import Window
 
+w = 300
+h = 200
+
 {- zips two lists -}
 zip : List a -> List b -> List (a,b)
 zip xs ys =
@@ -12,17 +15,46 @@ zip xs ys =
 
     (_, _) ->
         []
+        
+linspace : Float -> Float -> Int -> List Float
+linspace a b n =
+  List.map (\x -> (toFloat x) * (b - a) / ((toFloat n) - 1.0) + a) [0..(n-1)]
+        
+border : Form
+border =
+  path [ (w,h), (w,-h), (-w,-h), (-w,h), (w,h) ]
+    |> traced (solid blue)
+    
+   
+range : List Float -> (Float, Float)
+range x = (Maybe.withDefault 0.0 (List.minimum x), Maybe.withDefault 1.0 (List.maximum x))
+    
+
+axis : Int -> List Float -> List Float
+axis len data =
+  let
+    (a, b) = range data
+    l = toFloat len
+  in
+    List.map (\x -> l*(x-a)/b - l) data
+  
 
 {- generate fake data -}
-t = [0,10,20,30,40,50,60,70,80,90]
-x = [10,30,50,30,10,20,30,20,15,10]
-data = zip t x
+t = linspace -6.28 6.28 1000
+x = List.map sin t
+data = zip (axis w t) (axis h x)
 
 {- draws a point at the given location -}
-point x y =
-    circle 4 |> filled black |> move (x, y)
+point x y color =
+    circle 4 |> filled color |> move (x, y)
 
-points = List.map (\(x,y) -> point x y) data
+points = List.map (\(x,y) -> point x y black) data
+corners = [ point -w -h red
+          , point w h red
+          , point -w h red
+          , point w -h red]
+
 
 main = 
-    collage 200 200 points
+    collage (3*w) (3*h) (corners ++ points ++ [border])
+
